1e7f8698f8a777e5086c99a7dadf957a519c6538
Christopher Perry <c.perry@icloud.com>
2018-10-24 20:32:55 -0700

    save2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = props =>{
+
+
+};
+
+class NumberOfStudents extends React.Component{
+
+constructor(props){
+    super(props);
+    this.state = {
+        enrolledStudents: 87,
+        waitlistedStudents: 8
+
+    }
+}
+
+}
